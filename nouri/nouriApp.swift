0f2d82ddec84fb6fa2b1b2c0a42fd2ff14d6c79d
Louise <louise.cch@gmail.com>
2025-12-17 16:23:21 +0000

    Initial Commit

---
+++ nouri/nouriApp.swift
@@ -0,0 +1,17 @@
+//
+//  nouriApp.swift
+//  nouri
+//
+//  Created by Chan Ching Hei on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct nouriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nouriUITests/nouriUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  nouriUITestsLaunchTests.swift
+//  nouriUITests
+//
+//  Created by Chan Ching Hei on 17/12/2025.
+//
+
+import XCTest
+
+final class nouriUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
